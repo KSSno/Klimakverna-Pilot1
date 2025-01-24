c8bdb214a7e9f2edd22a29092aabc27d934dc1aa
shamly majeed <shamlym@met.no>
2025-01-24 12:05:56 +0100

    make config files for different types of inputs and fillup the metadata accordingly .

--- workflow/rules/testcase_7.smk
+++ workflow/rules/testcase_7.smk
@@ -8,6 +8,9 @@ import json
 config =config["testcase_7"]
 ATTRIBUTES = config["attributes"]
 VARIABLES = config["variables_pr"]
+INDEX = config["index_tasmax20ge"]
+MEAN = config["30_year_mean"]
+
 UPDATED_NC_OUT = config["updated_nc"]
 INPUT_DIR = config["input_nc"]
 INPUT_BASE = config["input_base"]
@@ -16,24 +19,25 @@ with open(INPUT_DIR, "r") as f:
 
 # Extract the list from the "file_pattern" key
 file_pattern_list = data["file_pattern"]
-
 all_file_paths = []
 
-for pattern in file_pattern_list:
+for pattern_type,pattern  in file_pattern_list.items():
     matching_files = glob.glob(pattern, recursive=True)
+
     for file_path in matching_files:
         struct = os.path.relpath(file_path, INPUT_BASE)
         file_base, _ = os.path.splitext(struct)  # Discard the extension
         all_file_paths.append(file_base)  # Append the file name without extension
         
-print(all_file_paths)
 
 rule metadata_filling:
     input:
-        f"{INPUT_BASE}{{filename}}.nc4",
+        lambda wildcards: glob.glob(f"{INPUT_BASE}{wildcards.filename}.nc4") or glob.glob(f"{INPUT_BASE}{wildcards.filename}.nc"),
         ATTRIBUTES,
-        VARIABLES
-
+        VARIABLES,
+        INDEX,
+        MEAN,
+        INPUT_DIR
     output:
         nc_output=os.path.join(UPDATED_NC_OUT, "{filename}.nc4"),
     script:
--- workflow/scripts/metadata_filling.py
+++ workflow/scripts/metadata_filling.py
@@ -2,11 +2,37 @@ from netCDF4 import Dataset
 import json
 from shutil import copyfile
 from datetime import date
+import fnmatch
 
-def update_metadata(input_file, output_file, global_attributes, variables):
+modified = False
+
+
+def update_metadata(input_file, output_file, global_attributes, attributes_variable_pr,attributes_variable_tasmax20ge,variable_diff_norheatwave, file_pattern_list):
     # Copy input file to the output location before editing
     copyfile(input_file, output_file)
-    modified=False
+    print(input_file)
+    add_common_global_attribute( output_file, global_attributes)
+    for  type, pattern  in file_pattern_list.items():
+        if (fnmatch.fnmatch(input_file, pattern)):
+            if type=="pr" :
+                add_attributes_variables(output_file, "pr", attributes_variable_pr)
+    
+            if type=="tasmax":
+                add_attributes_variables(output_file,"tasmax" , attributes_variable_tasmax20ge)
+
+            if type=="diff-norheatwave":
+                add_attributes_variables(output_file,"diff-norheatwave" , variable_diff_norheatwave)  
+    global modified
+    if modified:
+        with Dataset(output_file, "r+") as nc:
+            today = date.today().isoformat()
+            nc.setncattr("date_metadata_modified", today)
+            print(f"Updated date {today}")
+            modified = False
+
+
+def add_common_global_attribute(output_file,  global_attributes):
+    global modified 
 
     # Open the copied file in 'r+' mode to edit in place
     with Dataset(output_file, "r+") as nc:
@@ -15,27 +41,36 @@ def update_metadata(input_file, output_file, global_attributes, variables):
             if key not in nc.ncattrs():
                 nc.setncattr(key, value) 
                 modified=True
-                print(f"Updated metadata {key}:{value}")
-        # if "pr" in nc.variables:
-        #     pr_var = nc.variables['pr']
-        #     for key, value in variables.items():
-        #         if key not in pr_var.ncattrs():
-        #             pr_var.setncattr(key, value)
-        #             modified=True
-        #             print(f"Updated variables {key}:{value}")    
-        if modified:
-            today = date.today().isoformat()
-            nc.setncattr("date_metadata_modified", today)
-            print(f"Updated date {today}")
+                print(f"Updated global metadata {key}:{value}")   
+        
 
-def print_global_attributes(nc_file):
-    # Open the NetCDF file in read mode
-    with Dataset(nc_file, "r") as nc:
-        # Iterate through all global attributes
-        print("Global Attributes in the NetCDF file:")
-        for attr_name in nc.ncattrs():
-            attr_value = getattr(nc, attr_name)
-            print(f"{attr_name}: {attr_value}")
+def add_attributes_variables(output_file,variable_name, variable_attributes):
+    global modified 
+
+    with Dataset(output_file, "r+") as nc:
+    # Check and update metadata attributes
+        for key, value in variable_attributes.get("global", {}).items():
+            if key not in nc.ncattrs():
+                nc.setncattr(key, value) 
+                modified=True
+                print(f"Updated global attribute {variable_name} {key}:{value}")
+            if variable_name in nc.variables:
+                pr_var = nc.variables[variable_name]
+                for key, value in variable_attributes.get(variable_name, {}).items():
+                    if key not in pr_var.ncattrs():
+                        pr_var.setncattr(key, value)
+                        modified=True
+                        print(f"Updated variables {variable_name} {key}:{value}")    
+
+
+# def print_global_attributes(nc_file):
+#     # Open the NetCDF file in read mode
+#     with Dataset(nc_file, "r") as nc:
+#         # Iterate through all global attributes
+#         print("Global Attributes in the NetCDF file:")
+#         for attr_name in nc.ncattrs():
+#             attr_value = getattr(nc, attr_name)
+#             print(f"{attr_name}: {attr_value}")
 
 
 if __name__ == "__main__":
@@ -43,19 +78,36 @@ if __name__ == "__main__":
         input_file = snakemake.input[0]
         output_file = snakemake.output[0]
         attributes = snakemake.input[1]
-        variables = snakemake.input[2]
+        variable_pr = snakemake.input[2]
+        variable_tasmax20ge = snakemake.input[3]
+        variable_diff_norheatwave = snakemake.input[4]
+        pattern_list = snakemake.input[5]
+
+
+       
     except NameError:
         # Default values for standalone testing
         input_file = "example.nc"
         output_file = "example_updated.nc"
         attributes = "attributes.json"
-        variables="variables.json"
+        variable_pr="variables.json"
+        variable_diff_norheatwave="variable_diff_norheatwave.json"
+        pattern_list= "pattern_list.json"
     # Load global attributes from JSON
     with open(attributes, "r") as f:
         global_attributes = json.load(f)
-    with open(variables, "r") as f:
-        variables = json.load(f)
-    
+    with open(variable_pr, "r") as f:
+        attributes_variable_pr = json.load(f)
+    with open(variable_tasmax20ge, "r") as f:
+        attributes_variable_tasmax20ge = json.load(f)
+    with open(variable_diff_norheatwave, "r") as f:
+        attributes_variable_diff_norheatwave = json.load(f)    
+    with open(pattern_list, "r") as f:
+        data = json.load(f)
+    # Extract the list from the "file_pattern" key
+    file_pattern_list = data["file_pattern"]
+    # Extract the list from the "file_pattern" key
     # print_global_attributes(input_file)
-    update_metadata(input_file, output_file, global_attributes, variables)
+    update_metadata(input_file, output_file, global_attributes, attributes_variable_pr,attributes_variable_tasmax20ge,attributes_variable_diff_norheatwave, file_pattern_list)
     # print_global_attributes(output_file)
+
