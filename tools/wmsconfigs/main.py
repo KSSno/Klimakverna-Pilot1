c343e5117ab6cab9c0b590719e3ba8555ac716ce
Ketil Tunheim <ketil.tunheim@met.no>
2025-06-05 15:53:09 +0200

    Lint wmsconfig

--- tools/wmsconfigs/main.py
+++ tools/wmsconfigs/main.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3 
+#!/usr/bin/python3
 
 import pandas as pd
 import json
 import yaml
+
 
 # setup style list for one variable in a collection
 def style_config_from_variable(collection, varname, palettes):
@@ -10,35 +11,37 @@ def style_config_from_variable(collection, varname, palettes):
     var_styles = []
     for palette in palettes:
         # parse colors into list
-        if 'colors' not in palette:
-            print(f'Skipping {varname} in {collection} - no colors defined for palette')
+        if "colors" not in palette:
+            print(f"Skipping {varname} in {collection} - no colors defined for palette")
             return None
 
-        colors = [x.strip() for x in palette['colors'].split(',')]
+        colors = [x.strip() for x in palette["colors"].split(",")]
 
         # parse intervals into list
-        if 'intervals' not in palette:
-            print(f'Skipping {varname} in {collection} - no intervals defined for palette')
+        if "intervals" not in palette:
+            print(
+                f"Skipping {varname} in {collection} - no intervals defined for palette"
+            )
             return None
 
-        intervals = [float(x) for x in palette['intervals'].split(',')]
+        intervals = [float(x) for x in palette["intervals"].split(",")]
 
         # open intervals are default
-        open_lowest_interval = palette.get('openLowestInterval', True)
-        open_highest_interval = palette.get('openHighestInterval', True)
+        open_lowest_interval = palette.get("openLowestInterval", True)
+        open_highest_interval = palette.get("openHighestInterval", True)
 
         # add variable names
-        variables = palette.get('variables', [])
+        variables = palette.get("variables", [])
 
         # make one style per variable
         for variable in variables:
             # define style config for given variables
             style_config = {
-                'name': variable,
-                'colors': colors,
-                'intervals': intervals,
-                'open_lowest_interval': open_lowest_interval,
-                'open_highest_interval': open_highest_interval,
+                "name": variable,
+                "colors": colors,
+                "intervals": intervals,
+                "open_lowest_interval": open_lowest_interval,
+                "open_highest_interval": open_highest_interval,
             }
             # append to config
             var_styles.append(style_config)
@@ -49,17 +52,17 @@ def style_config_from_variable(collection, varname, palettes):
 # for one file of objects in a collection, extract the information needed for WMS config
 def wms_config_from_file(input_dir, collection):
     # read object config from file
-    filepath = input_dir + collection + '.json'
-    with open(filepath, 'r', encoding='utf-8') as f:
+    filepath = input_dir + collection + ".json"
+    with open(filepath, "r", encoding="utf-8") as f:
         object_config = json.load(f)
 
     # loop over objects in the config
     elements = []
     for varname, config in object_config.items():
         # palettes defined at all?
-        palettes = config.get('palettes', None)
+        palettes = config.get("palettes", None)
         if palettes is None:
-            print(f'Skipping {varname} in {collection} - no palettes defined')
+            print(f"Skipping {varname} in {collection} - no palettes defined")
             continue
 
         # TODO: deduce the pattern based on collection config and variable name
@@ -70,20 +73,23 @@ def wms_config_from_file(input_dir, collection):
             continue
 
         # define variable level config and append to elements
-        elements.append({
-            'pattern': '',
-            'base_netcdf_directory': '',
-            'module': 'mapgen.modules.generic_quicklook',
-            'module_function': 'generic_quicklook',
-            'mapfiles_path': '/mapfiles',
-            'styles': var_styles,
-        })
+        elements.append(
+            {
+                "pattern": "",
+                "base_netcdf_directory": "",
+                "module": "mapgen.modules.generic_quicklook",
+                "module_function": "generic_quicklook",
+                "mapfiles_path": "/mapfiles",
+                "styles": var_styles,
+            }
+        )
 
     return elements
 
-#
+
+# write WMS config for all collections
 def produce_wms_config(input_dir, output_dir, collections):
-    # prepate config list for all objects
+    # prepare config list for all objects
     wms_config = []
 
     # loop over collections and parse their object config files
@@ -93,20 +99,26 @@ def produce_wms_config(input_dir, output_dir, collections):
             wms_config.extend(collection_config)
 
     # write to YAML file
-    output_filename = output_dir + 'wms_config.yaml'
-    with open(output_filename, 'w', encoding='utf-8') as f:
-        yaml.dump(wms_config, f, default_flow_style=None, allow_unicode=True, explicit_start=True)
+    output_filename = output_dir + "wms_config.yaml"
+    with open(output_filename, "w", encoding="utf-8") as f:
+        yaml.dump(
+            wms_config,
+            f,
+            default_flow_style=None,
+            allow_unicode=True,
+            explicit_start=True,
+        )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # directories
-    input_dir = 'tools/kinconfigs/output/'
-    output_dir = 'tools/wmsconfigs/output/'
+    input_dir = "tools/kinconfigs/output/"
+    output_dir = "tools/wmsconfigs/output/"
 
     # collections that should be published through WMS
     collections = [
-        'ReferenceIndices',
-        'ClimateStatistics',
+        "ReferenceIndices",
+        "ClimateStatistics",
     ]
 
     produce_wms_config(input_dir, output_dir, collections)
